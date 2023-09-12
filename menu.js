90b80f10208f084a6c4da26eeab203467eeb11b9
guilherme6554 <138627954+guilherme6554@users.noreply.github.com>
2023-09-12 20:33:00 -0300

    menu

--- menu.js
+++ menu.js
@@ -2,21 +2,26 @@ const input = require('readline-sync').question;
 
 function menu (){
     
-    
+    let o = -1  
 
-while(o<=7,o>=0 )
-        
-        console.log('1-Cadastro de aluno')
-        console.log('2-listar todos alunos')
-        console.log('3-listar aluno específico')
-        console.log('4-Atualizar aluno')
-        console.log('5-excluir aluno')
-        console.log('6-média ')
-        console.log('7- main')
-        console.log('0-sair')
-        
-        
-        let o = input('opcao:')
+
+
+
+   while(o>7 || o<0 ){
+
+    
+    console.log('1-Cadastro de aluno')
+    console.log('2-listar todos alunos')
+    console.log('3-listar aluno específico')
+    console.log('4-Atualizar aluno')
+    console.log('5-excluir aluno')
+    console.log('6-média ')
+    console.log('7- main')
+    console.log('0-sair')
+    
+    
+    o = input('opcao:')
+}
 
         
         }
